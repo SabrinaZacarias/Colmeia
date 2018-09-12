299d385d57c579fc9c73321cc2252d9cab7fe68b
Sabrina <sabrina.zacs@gmail.com>
2018-09-12 14:11:52 -0300

    arrumar erros no commit anterior

---
+++ ong.js
@@ -0,0 +1,11 @@
+let modal = document.querySelector(".modal");
+ 
+
+function fechar(){
+    onclick = modal.style.display='none';
+    
+    }
+function clicar(){
+onclick = modal.style.display='block';
+
+}
